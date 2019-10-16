2adc197ea8787a676b406a3bb0ddaaceac8415be
guozenghuang <guozh94@outlook.com>
2019-10-16 22:18:29 +0800

    TCP客户端

--- TCP客户端.py
+++ TCP客户端.py
@@ -5,7 +5,7 @@ tcp_client_socket = socket(AF_INET, SOCK_STREAM)
 
 # 目的信息
 server_ip = input("请输入服务器ip:")
-server_port = input("请输入服务器port:")
+server_port = int(input("请输入服务器port:"))
 
 # 链接服务器
 tcp_client_socket.connect((server_ip, server_port))
